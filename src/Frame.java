6324bbd85378a654dd430e8509438fb3b728a900
Aditya Chattopadhyay <adichattopadhyay@gmail.com>
2019-01-21 14:49:36 -0500

    Changed Tile Constructor, added mouselistener

--- src/Frame.java
+++ src/Frame.java
@@ -2,47 +2,78 @@ import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 import tiles.Tile;
 
 import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Arrays;
-public class Frame extends JFrame{
+public class Frame extends JFrame implements MouseListener{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4983727800375272097L;
-
+	
+	int clicker =0; 
+	
 	public Frame(int sizeX,int sizeY) {
 		setSize(sizeX,sizeY);
 		setTitle("A*Pathfinding");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setUpGame(sizeX,sizeY);
+		setUpGrid(sizeX,sizeY);
 		
 		setResizable(false);
 		setVisible(true);
+		addMouseListener(this);
 	}
 	
-	private void setUpGame(int sizeX,int sizeY) {
+	private void setUpGrid(int sizeX,int sizeY) {
 		getContentPane().setLayout(new GridLayout(sizeY/50,sizeX/50));
 		int tileNum= 0;
-		for(int i = 0; i<(sizeX*sizeY)/(50*50); i++) {
-			getContentPane().add(new Tile());
-			tileNum++;
+		for(int i = 0; i<sizeX/50; i++) {
+			for(int j = 0; j<sizeY/50; j++) {
+				getContentPane().add(new Tile(i*50,j*50));
+				tileNum++;
+			}
 		}
 		int[][] tileArray = new int[(int) Math.sqrt(tileNum)][(int) Math.sqrt(tileNum)];
-		System.out.println(Arrays.deepToString(tileArray));
+		System.out.println(tileNum);
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Frame(1000,1000);
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		/*Tile tile = new Tile();
+		if(clicker ==0) {
+			tile.startPlacement();
+			tile.setBackground(Color.red);
+			clicker =1;
+			System.out.println(clicker);
+		}
+		else {
+			tile.endPlacement();
+			clicker =2;
+		}*/
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
 	}
 
 }
--- src/tiles/Tile.java
+++ src/tiles/Tile.java
@@ -2,17 +2,24 @@ package tiles;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
 public class Tile extends JLabel{
-		
+	
+	int x,y;
+	int clicker = 0;
 	private Color faceColor = Color.WHITE;
 	private final static Dimension size = new Dimension(50,50);
 	
-	public Tile()
+	public Tile(int x, int y)
 	{
+		this.x = x;
+		this.y = y;
 		setBorder(BorderFactory.createLineBorder(Color.black));
 		setMinimumSize(size);
 		setMaximumSize(size);
@@ -21,8 +28,36 @@ public class Tile extends JLabel{
 		setBackground(Color.white);
 	}
 	
-	protected void setFaceColor(Color c) {
+	public void setFaceColor(Color c) {
 		this.faceColor = c;
+		setBackground(faceColor);
 	}
+	
+	
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+	public void startPlacement() {
+		if(clicker ==0) {
+			System.out.println("Placing start.");
+			setFaceColor(Color.red);
+			setBackground(Color.red);
+		}
+	}
+	
+	public void endPlacement() {
+		if(clicker==1) {
+			System.out.println("Placing end.");
+			setFaceColor(Color.BLUE);
+			clicker = 2;
+		}
+	}
+
 	
 }
